e5e60ac79aa4d907a021b47f2876340ac1684ee1
Vinicius Dias <carlosv775@gmail.com>
2019-10-02 23:02:28 -0300

    Definindo status de erro para as respostas

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -5,6 +5,7 @@ namespace App\EventListener;
 use App\Entity\HypermidiaResponse;
 use App\Helper\EntityFactoryException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -17,6 +18,7 @@ class ExceptionListener implements EventSubscriberInterface
             KernelEvents::EXCEPTION => [
                 ['handleEntityFactoryException', 1],
                 ['handle404Exception', 0],
+                ['handleGenericException', -1]
             ],
         ];
     }
@@ -27,7 +29,9 @@ class ExceptionListener implements EventSubscriberInterface
             return;
         }
 
-        $event->setResponse(HypermidiaResponse::fromError($event->getException())->getResponse());
+        $response = HypermidiaResponse::fromError($event->getException())->getResponse();
+        $response->setStatusCode(Response::HTTP_NOT_FOUND);
+        $event->setResponse($response);
     }
 
     public function handleEntityFactoryException(GetResponseForExceptionEvent $event)
@@ -36,6 +40,13 @@ class ExceptionListener implements EventSubscriberInterface
             return;
         }
 
-        $event->setResponse(HypermidiaResponse::fromError($event->getException())->getResponse());
+        $response = HypermidiaResponse::fromError($event->getException())->getResponse();
+        $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+        $event->setResponse($response);
+    }
+
+    public function handleGenericException(GetResponseForExceptionEvent $event)
+    {
+        $event->setResponse(HypermidiaResponse::fromError(new \Exception('Um erro inesperado ocorreu'))->getResponse());
     }
 }
