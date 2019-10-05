3f591c22dd55b8ab7c7cb7e661529c7134b9dc54
Vinicius Dias <carlosv775@gmail.com>
2019-10-05 16:52:29 -0300

    Aula 4
    Testes

---
+++ tests/EspecialidadesWebTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Tests;
+
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class EspecialidadesWebTest extends WebTestCase
+{
+    public function testGaranteQueRequisicaoFalhaSemAutenticacao()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/especialidades');
+
+        self::assertEquals(
+            401,
+            $client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testGaranteQueEspecialidadesSaoListadas()
+    {
+        $client = static::createClient();
+        $token = $this->login($client);
+        $client->request('GET', '/especialidades', [], [], [
+            'HTTP_AUTHORIZATION' => "Bearer $token"
+        ]);
+
+        $resposta = json_decode($client->getResponse()->getContent());
+        self::assertTrue($resposta->success);
+    }
+
+    public function testInsereEspecialidade()
+    {
+        $client = static::createClient();
+        $token = $this->login($client);
+        $client->request('POST', '/especialidades', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_AUTHORIZATION' => "Bearer $token"
+        ], json_encode([
+            'descricao' => 'Teste'
+        ]));
+
+        self::assertEquals(201, $client->getResponse()->getStatusCode());
+    }
+
+    private function login(KernelBrowser $client): string
+    {
+        $client->request(
+            'POST',
+            '/login',
+            [],
+            [],
+            [
+            'CONTENT_TYPE' => 'application/json'
+            ],
+            json_encode([
+                'username' => 'username',
+                'password' => 'password'
+            ])
+        );
+
+        return json_decode($client->getResponse()->getContent())
+            ->access_token;
+    }
+}
